98aadafdbccf83a0ea42b083b12d487fbee32439
eVgenius13 <panfilov-e@yandex.ru>
2022-10-07 11:36:47 +0700

    Урок 1 начали | познакомились с var val

---
+++ app/src/main/java/com/example/k_2135_2136/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.k_2135_2136
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var field1 = 5
+        val field2 = 5
+    }
+}
+
+class Test constructor(var int: Int, var){
+
+}
